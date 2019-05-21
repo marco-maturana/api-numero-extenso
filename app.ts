63a0c501ce23a3b290202c6ba2bad10f61590dd7
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-05-21 15:40:01 -0400

    configuracoes basicas para ambiente de teste

--- app.ts
+++ app.ts
@@ -2,8 +2,6 @@ import server from "./src/server";
 
 const port = process.env.APP_PORT || 3000;
 
-server.listen(port, () => {
+export default server.listen(port, () => {
   console.log(`Server is running on the port ${port}!`);
 });
-
-export default server;
---
+++ tests/specs/example.test.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("teste", function () {
+  it("teste", async function () {
+    expect(0).to.equal(0);
+  });
+});
