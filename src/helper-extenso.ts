f045664dae22f1a14588129f24266412a77f1839
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-05-22 08:59:54 -0400

    permitido numeros entre -100 e 100 na nova implementacao

--- src/helper-extenso.ts
+++ src/helper-extenso.ts
@@ -11,8 +11,41 @@ const unidades = [
   "nove"
 ];
 
+const dezena = [
+  "dez",
+  "onze",
+  "doze",
+  "treze",
+  "quatorze",
+  "quinze",
+  "dezesseis",
+  "dezessete",
+  "dezoito",
+  "dezenove",
+  "vinte",
+  "trinta",
+  "quarenta",
+  "cinquenta",
+  "sessenta",
+  "setenta",
+  "oitenta",
+  "noventa"
+];
+
 function menor10 (valor: number) {
   return unidades[valor];
+}
+
+function menor100 (valor: number): string {
+  if (valor < 10) return menor10(valor);
+  if (valor < 20) return dezena[valor - 10];
+
+  const extensoUnidade = menor10(valor % 10);
+  const extensoDezena = dezena[(valor - valor % 10) / 10 + 8];
+
+  return extensoUnidade !== "zero"
+    ? `${extensoDezena} e ${extensoUnidade}`
+    : extensoDezena;
 }
 
 export default function (valor: number | string): string {
@@ -24,6 +57,7 @@ export default function (valor: number | string): string {
   let extenso;
 
   if (numeroPositivo < 10) extenso = menor10(numeroPositivo);
+  else if (numeroPositivo < 100) extenso = menor100(numeroPositivo);
   else extenso = "";
 
   return negativo && extenso ? "menos " + extenso : extenso;
