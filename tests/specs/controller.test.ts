4b55099c2e18f5e3dcfa9d1ae5bf5148de972f95
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-05-24 19:14:40 -0400

    alterado a origem dos dados dos testes
    os testes passaram a obter dados aleatorios a partir do modulo para geracao
    desses dados e da biblioteca faker.js

---
+++ tests/specs/controller.test.ts
@@ -0,0 +1,268 @@
+import { expect } from "chai";
+import { random } from "faker";
+import axios from "../axios";
+import dadosTeste from "../dados-teste";
+
+describe("utilizando algoritmo da biblioteca extenso.js", function () {
+  describe("numeros inválidos", function () {
+    it("número menor que -99999", async function () {
+      let resposta;
+
+      try {
+        resposta = await axios.get("/" + random.number({
+          min: -1000000,
+          max: -100000
+        }));
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("O valor deve ser maior ou igual a -99999");
+    });
+
+    it("número maior que 99999", async function () {
+      let resposta;
+
+      try {
+        resposta = await axios.get("/" + random.number({
+          min: 100000,
+          max: 1000000
+        }));
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("O valor deve ser menor ou igual a 99999");
+    });
+
+    it("número decimal", async function () {
+      let resposta;
+
+      try {
+        resposta = await axios.get("/" + random.number({
+          min: -99999,
+          max: 9999,
+          precision: 0.01
+        }));
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("Deve ser informado apenas números inteiros!");
+    });
+
+    it("valor monetário", async function () {
+      let resposta;
+
+      try {
+        const monetario = random.number({
+          min: -99999,
+          max: 9999,
+          precision: 0.01
+        }).toString().replace(".", ",");
+
+        resposta = await axios.get(`/${monetario}`);
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("Apenas números serão aceitos!");
+    });
+  });
+
+  it("números inteiros com apenas uma casa", async function () {
+    const valorTeste = dadosTeste("unidade");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+
+  it("números inteiros com duas casas", async function () {
+    const valorTeste = dadosTeste("dezena");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+
+  it("números inteiros com três casas", async function () {
+    const valorTeste = dadosTeste("centena");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+
+  it("números inteiros com quatro ou cinco casas", async function () {
+    const valorTeste = dadosTeste("milhar");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+});
+
+describe("utilizando o próprio algoritmo", function () {
+  describe("numeros inválidos", function () {
+    it("número menor que -99999", async function () {
+      let resposta;
+
+      try {
+        resposta = await axios.get("/v2/" + random.number({
+          min: -1000000,
+          max: -100000
+        }));
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("O valor deve ser maior ou igual a -99999");
+    });
+
+    it("número maior que 99999", async function () {
+      let resposta;
+
+      try {
+        resposta = await axios.get("/v2/" + random.number({
+          min: 100000,
+          max: 1000000
+        }));
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("O valor deve ser menor ou igual a 99999");
+    });
+
+    it("número decimal", async function () {
+      let resposta;
+
+      try {
+        resposta = await axios.get("/v2/" + random.number({
+          min: -99999,
+          max: 9999,
+          precision: 0.01
+        }));
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("Deve ser informado apenas números inteiros!");
+    });
+
+    it("valor monetário", async function () {
+      let resposta;
+
+      try {
+        const monetario = random.number({
+          min: -99999,
+          max: 9999,
+          precision: 0.01
+        }).toString().replace(".", ",");
+
+        resposta = await axios.get(`/v2/${monetario}`);
+      } catch (error) {
+        resposta = error.response;
+      }
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.data.errors).to.include("Apenas números serão aceitos!");
+    });
+  });
+
+  it("números inteiros com apenas uma casa", async function () {
+    const valorTeste = dadosTeste("unidade");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/v2/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+
+  it("números inteiros com duas casas", async function () {
+    const valorTeste = dadosTeste("dezena");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/v2/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+
+  it("números inteiros com três casas", async function () {
+    const valorTeste = dadosTeste("centena");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/v2/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+
+  it("números inteiros com quatro ou cinco casas", async function () {
+    const valorTeste = dadosTeste("milhar");
+
+    let resposta;
+
+    try {
+      resposta = await axios.get(`/v2/${valorTeste.valor}`);
+    } catch (error) {
+      resposta = error.response;
+    }
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.data.extenso).to.equal(valorTeste.extenso);
+  });
+});
